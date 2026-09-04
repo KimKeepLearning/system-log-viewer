025df15b08709cb28f0183fc4e195289a5605d49
Yennefer <18261195537@163.com>
2026-09-04 14:53:02 +0800

    chore: bump to 1.5.2

--- src/renderer/src/lib/log-parser.ts
+++ src/renderer/src/lib/log-parser.ts
@@ -84,7 +84,6 @@ const parseStandardLogLine = (line: string): IUserLog | null => {
   // paths, `[0831/061736]` sampler stamps and bare `[dbus/o` truncations out.
   let tag: string | undefined;
   const tagMatch = message.match(/^"?\[([A-Za-z][A-Za-z0-9 _.-]{1,30})\]\s+(.*)$/);
-  console.log("tag match", tagMatch, message);
   if (tagMatch) {
     tag = tagMatch[1];
     message = tagMatch[2];
